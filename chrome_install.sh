16783da4eaddf9e9deb7ae1d8bf9f0a8574c66c9
kisan117 <k09731159@gmail.com>
2025-04-28 21:24:13 +0530

    Create chrome_install.sh

---
+++ chrome_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Install Google Chrome and Chromedriver
+apt-get update -y
+apt-get install wget curl unzip -y
+apt-get install google-chrome-stable -y
+apt-get install -y chromium-driver
