b879ca323c5ebe8de0ea68bb965288369d4d0045
kisan117 <k09731159@gmail.com>
2025-04-29 07:04:02 +0530

    Delete chrome_install.sh

--- chrome_install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Install Google Chrome and Chromedriver
-apt-get update -y
-apt-get install wget curl unzip -y
-apt-get install google-chrome-stable -y
-apt-get install -y chromium-driver
